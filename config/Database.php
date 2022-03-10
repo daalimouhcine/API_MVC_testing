03b0c47a70c72af88b1b2fe63a23c8d3d6769a43
Mouhcine Daali <daalim277@gmail.com>
2022-03-10 11:22:07 +0100

    end with the connection of database

--- config/Database.php
+++ config/Database.php
@@ -1,8 +1,23 @@
 <?php
     
     class Database {
-        private $name = '';
-        private $root;
-        private $password;
+        private $host = 'localhost';
+        private $dbname = 'api_test';
+        private $root = 'root';
+        private $password = '';
         private $conn;
+
+        // DB connect
+        public function connect() {
+            $this->conn = null;
+
+            try {
+                $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->root, $this->password);
+                $this->conn->getAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            } catch(PDOException $e) {
+                echo 'Connect Error: ' . $e->getMessage(); 
+            }
+            return $this->conn;
+        }
     }

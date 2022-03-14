66d9dcce50e8ec2ee1f358d174e6c2c4e0dd44b3
Mouhcine Daali <daalim277@gmail.com>
2022-03-14 12:00:27 +0100

    delete and add posts class with the method read

---
+++ api/Posts.php
@@ -0,0 +1,17 @@
+<?php
+    require_once '../../models/Post.php';
+
+    class Posts {
+        
+        public function __construct() {
+            // $this->postModel = new Post;
+            echo 'hello';
+        }
+
+        public function read() {
+            // $posts = $this->postModel->readPosts();
+            // echo json_encode($this->postModel->readPosts());
+            echo 'hello';
+        }
+
+    }
